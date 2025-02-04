3c2d2a722003fc3f56f6b541b639ce4da237ba0d
Clement Chong <119944151+clementtech@users.noreply.github.com>
2025-02-04 09:08:39 +0000

    Added Valid Link Checker Separated out the link checker into a separate function for PyTest

--- converter.py
+++ converter.py
@@ -4,16 +4,33 @@
 # Convert the Link Format as below to get Direct Download Link Format
 # https://drive.google.com/uc?export=download&id=YourUniqueFileCode
 
+import sys
 
 def main():
     
-    link_input = str(input("Enter the Shared Link: "))
+    print(convert(str(input("Enter the Shared Link: "))))
 
-    unique_code = link_input.strip().split('/')[5]
+def convert(link):
 
-    direct_link = f"https://drive.google.com/uc?export=download&id={unique_code}"
+    if "drive.google.com" not in link:
+        sys.exit("Invalid Link Format")
 
-    print(f"Direct Download Link: {direct_link}")
+    elif "usp=sharing" not in link:
+        sys.exit("File is not Publicly Shared")
+    
+    unique_code = code(link)
+
+    return f"Direct Download Link:\nhttps://drive.google.com/uc?export=download&id={unique_code}"
+
+
+def code(code):
+
+    try:
+        return code.strip().split('/')[5]
+
+    except IndexError:
+        sys.exit("Invalid Link Format")
+
 
 if __name__ == '__main__':
     main()
