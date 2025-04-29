daaaf0c0e4e15689c781d0b9645738e40e8010d3
clementtech <119944151+clementtech@users.noreply.github.com>
2025-04-29 20:29:22 +0800

    Update converter.py
    Added comment to the program

--- converter.py
+++ converter.py
@@ -1,28 +1,48 @@
+# Import library into the program
+# Pyperclip is used to copy/paste URL to clipboard
 import pyperclip
+# Regular Expression (re) is used to check if the link is valid and extract id
 import re
+# sys is used to exit the program when invalid link is provided
 import sys
 
+# Regex, link format that the re should cross check with the input to ensure the link is valid
 link_format =  r"^https://drive.google.com/file/d/.+/view\?usp=.+$"
 
+# Request link input from user and convert it to string
 link_input = str(input("Link: "))
 
+# re.search(template, input)
+# If the input matches with the format
+# Proceed with the program
 if re.search(link_format, link_input):
 
+    # Split out the front part of the link
     link_format_split_a = r"https://drive.google.com/file/d/"
 
+    # Left the back nonsense of the link
     link_split_a = re.split(link_format_split_a, link_input)[1]
 
+    # Split the last part of the link
     link_format_split_b = r"/view\?usp="
 
+    # The link should just remain the id which is what we want
     link_id = re.split(link_format_split_b, link_split_a)[0]
 
+    # Build the export link with the link id
     converted_link = f"https://drive.google.com/uc?export=download&id={link_id}"
 
+    # Copy the converted_link into user clipboard
     pyperclip.copy(converted_link)
 
+    # Paste the link into the user clipboard
     pyperclip.paste()
 
+    # Show success message to the user and display the converted link
     print(f"Copied link to clipboard: {converted_link}")
 
+# If link does not matches
 else: 
+
+    # Exit the program with the message "Invalid Link."
     sys.exit("Invalid Link.")
